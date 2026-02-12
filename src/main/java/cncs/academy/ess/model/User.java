5c5b71e0a186b1e182242100a407eae015328708
JoaoAfonsoSilva <124693185+JoaoAfonsoSilva@users.noreply.github.com>
2026-02-12 12:23:27 +0000

    feat: implemented password hashing with PBKDF2

--- src/main/java/cncs/academy/ess/model/User.java
+++ src/main/java/cncs/academy/ess/model/User.java
@@ -2,17 +2,22 @@ package cncs.academy.ess.model;
 
 public class User {
     private int id;
-    private String username;
-    private String password;
-    public User(int id, String username, String password) {
+    private final String username;
+    private final byte[] password;
+    private final byte[] salt;
+
+    public User(int id, String username, byte[] password, byte[] salt) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.salt = salt;
     }
-    public User(String username, String password) {
+    public User(String username, byte[] password, byte[] salt) {
         this.username = username;
         this.password = password;
+        this.salt = salt;
     }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -22,8 +27,9 @@ public class User {
     public String getUsername() {
         return username;
     }
-    public String getPassword() {
+    public byte[] getPassword() {
         return password;
     }
+    public byte[] getSalt() { return salt; }
 }
 
